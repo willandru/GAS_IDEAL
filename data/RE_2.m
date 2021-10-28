5146731e50681afab3e1d737b5f61bf0073681dc
willandru <willandru@hotmail.com>
2021-10-28 12:23:40 -0400

    Done¡¡ Latex article finished

--- data/RE_2.m
+++ data/RE_2.m
@@ -4,13 +4,28 @@ L=[15 14 13	12 11 10 9 8 7 6 5];
 P21=[8 8.3 9.1 9.6 10.5 11.4 13.4 15 17 19.1 23.2];
 P22=[8 8.2 8.9 10 10.8 11.3 13.3 14.9 17 19.8 23.6];
 P23=[8 8.6 9.3 10 10.5 11.3 12.5 15.1 17.1 19.1 23.7];
-P2=[P21; P22; P23];
-P2=mean(P2);
+A2=[P21; P22; P23];
+P2=mean(A2);
+DESVIACION= std(A2)'
 P2=P2.*(1.013e5);
+
+errAbsoluto= DESVIACION/sqrt(11); %Incertidumbre Experimental
+errRelativo= errAbsoluto.*100./P2';
+P21=P21.*(1.013e5);
+P22=P22.*(1.013e5);
+P23=P23.*(1.013e5);
+
 figure(2)
 
 L=L.*1e-9;
 L=L.^3
+Volume = L'
+Presion1=P21';
+Presion2=P22';
+Presion3=P23';
+MEAN=P2';
+table(Volume, Presion1, Presion2, Presion3)
+table(MEAN, DESVIACION, errAbsoluto, errRelativo)
 plot(P2,L,'*-b')
 
 L2=[10 11 12 13 14 15 9 8 7 6 5];
@@ -22,20 +37,20 @@ hold on;
 PA=PA.*(1e3);
 
 LA=LA.*1e-9;
-LA=LA.^3
+LA=LA.^3;
 plot(PA,LA,'--K')
 
 P23=[1208 1072 941 876 821 768 1301 1467 1681 1961 2340];
 L21=[10 11 12 13 14 15 9 8 7 6 5];
 hold on;
 
-LD=[5 6 7 8 9 10 11 12 13 14 15]
-PD=[2340 1961 1681 1467 1301 1208 1072 941 876 821 768]
+LD=[5 6 7 8 9 10 11 12 13 14 15];
+PD=[2340 1961 1681 1467 1301 1208 1072 941 876 821 768];
 
 PD=PD.*(1e3);
 
 LD=LD.*1e-9;
-LD=LD.^3
+LD=LD.^3;
 plot(PD,LD,'*Y')
 
 
@@ -48,4 +63,3 @@ ylabel('Volúmen [m³]');
 P2=P2';
 p2=p2';
 P23=P23';
-table(P2,p2,P23)
--- data/RE_31.m
+++ data/RE_31.m
@@ -6,12 +6,29 @@ T31=[300 362 421 481 540 602 661 719 780 838 900];
 T32=[300 362 420 481 543 598 660 717 781 838 900]; 
 T33=[300 362 421 483 540 598 659 721 783 842 900];
 
-TW1=[T31; T32; T33];
-TW1=mean(TW1);
-figure(3)
+a1=[T31; T32; T33];
+TW1=mean(a1);
+MEAN=TW1;
+DESVIACION= std(a1);
+errAbsoluto= DESVIACION/sqrt(11); %Incertidumbre Experimental
+errRelativo= errAbsoluto.*100./MEAN;
 
 L3=L3.*1e-9;
-L3=L3.^3
+L3=L3.^3;
+
+Volume= L3';
+Temperature1=T31';
+Temperature2=T32';
+Temperature3=T33';
+MEAN=MEAN';
+DESVIACION=DESVIACION';
+errAbsoluto=errAbsoluto';
+errRelativo=errRelativo';
+table(Volume, Temperature1, Temperature2, Temperature3)
+table(MEAN, DESVIACION, errAbsoluto, errRelativo)
+figure(3)
+
+
 plot(TW1, L3,'-*k')
 
 T3A=[300 331 361 391 420 450 270 241 209 181 151];
@@ -19,22 +36,22 @@ L3A=[10 11 12 13 14 15 9 8 7 6 5];
 
 hold on;
 L3A=L3A.*1e-9;
-L3A=L3A.^3
+L3A=L3A.^3;
 plot(T3A, L3A,'*r')
 
 T31D=[300 244 212 182 153 307 337 367 396 426 457];
 L31D=[9.8 8 7 6 5 10 11 12 13 14 15];
 hold on;
 L31D=L31D.*1e-9;
-L31D=L31D.^3
+L31D=L31D.^3;
 plot(T31D, L31D,'-*b')
 
-L=[5 6 7 8 9 10 11 12 13 14 15] 
-T=[320 383 446 510 572 638 702 765 831 891 957]
+L=[5 6 7 8 9 10 11 12 13 14 15] ;
+T=[320 383 446 510 572 638 702 765 831 891 957];
 
 hold on;
 L=L.*1e-9;
-L=L.^3
+L=L.^3;
 plot(T,L,'*-')
 
 title('Presión constante');
